9d8c8ff59713e5fcc11e397a636d49db5c598999
Niranjan Rao <niranjanhrao@gmail.com>
2016-12-14 16:18:57 +0100

    update flash.sh

--- bin/flash.sh
+++ bin/flash.sh
@@ -10,8 +10,8 @@ if [ "$1" == "-j" -a "${JLINK}x" != "x" ]; then
 else
   HOST=`uname`
   BLBASE=`dirname $0`/blhost
-  BLHOST=$([ "$HOST" == "Darwin" ] && echo "$BLBASE/mac/blhost" || echo "$BASE/linux/blhost")
+  BLHOST=$([ "$HOST" == "Darwin" ] && echo "$BLBASE/mac/blhost" || echo "$BLBASE/linux/amd64/blhost")
   ${BLHOST} -u -- flash-erase-all
-  [ $? -eq 0 ] && ${BLHOST} -u -- write-memory 0x0 ./BUILD/UBIRCH1/GCC_ARM/mbed-src-program.bin
+  [ $? -eq 0 ] && ${BLHOST} -u -- write-memory 0x0 ./BUILD/UBIRCH1/GCC_ARM/mbed-os-porting.bin
   [ $? -eq 0 ] && ${BLHOST} -u -- reset
 fi
