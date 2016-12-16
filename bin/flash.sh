8f518f963cf3fd66532c528f183a666ddbceeba6
Niranjan Rao <niranjanhrao@gmail.com>
2016-12-16 17:08:54 +0100

    Fix JLink for linux

--- bin/flash.sh
+++ bin/flash.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
-if [ "$1" == "-j" -a "${JLINK}x" != "x" ]; then
+BASE=`dirname $0`
+if [ "$1" = "-j" ]; then
   JLINK=`which JLinkExe`
-  if [ "$JLINK}"x == "x" ]; then
+  if [ "${JLINK}"x = "x" ]; then
     echo "JLinkExe not found, please ensure its in the PATH"
     exit 1
   else
-    ${JLINK} -if SWD -device MK82FN256xxx15 -speed 4000 ./flash.jlink
+    ${JLINK} -if SWD -device MK82FN256xxx15 -speed 4000 $BASE/flash.jlink
   fi
 else
   HOST=`uname`
-  BLBASE=`dirname $0`/blhost
+  BLBASE=$BASE/blhost
   BLHOST=$([ "$HOST" == "Darwin" ] && echo "$BLBASE/mac/blhost" || echo "$BLBASE/linux/amd64/blhost")
   ${BLHOST} -u -- flash-erase-all
   [ $? -eq 0 ] && ${BLHOST} -u -- write-memory 0x0 ./BUILD/UBIRCH1/GCC_ARM/mbed-os-porting.bin
