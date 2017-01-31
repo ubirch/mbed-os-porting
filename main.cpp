e840a22c0f8f87847e5969a1e1219953c45d5e31
Niranjan Rao <niranjanhrao@gmail.com>
2017-01-31 17:38:59 +0100

    add MQTT interface and MQTT test program

--- main.cpp
+++ main.cpp
@@ -1,113 +1,126 @@
-#include "mbed.h"
-#include "BME280.h"
-//#include "M66/M66ATParser/M66ATParser.h"
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Ian Craggs - initial API and implementation and/or initial documentation
+ *    Ian Craggs - make sure QoS2 processing works, and add device headers
+ *******************************************************************************/
+
+/**
+ This is a sample program to illustrate the use of the MQTT Client library
+ on the mbed platform.  The Client class requires two classes which mediate
+ access to system interfaces for networking and timing.  As long as these two
+ classes provide the required public programming interfaces, it does not matter
+ what facilities they use underneath. In this program, they use the mbed
+ system libraries.
+
+*/
+
+#define logMessage printf
+
+#define MQTTCLIENT_QOS2 1
+
 #include "M66Interface.h"
+#include "M66/MQTTNetwork.h"
+#include "MQTTmbed.h"
+#include "MQTTClient.h"
 
-DigitalOut led1(LED1);
+int arrivedcount = 0;
 
-BME280       env_sensor(I2C_SDA, I2C_SCL);
-M66Interface modem(GSM_UART_TX, GSM_UART_RX, GSM_PWRKEY, GSM_POWER, true);
-
-void led_thread(void const *args) {
-    while (true) {
-//        printf("BLINK\r\n");
-        led1 = !led1;
-        Thread::wait(1000);
-    }
-}
-
-void bme_thread(void const *args) {
-    while (true) {
-      printf("Temprature: %2.2f C :: Pressure: %04.2f hPa :: Humidity: %2.2f%%\r\n",
-         env_sensor.getTemperature(),
-         env_sensor.getPressure(),
-         env_sensor.getHumidity()
-         );
-      Thread::wait(5000);
-    }
-}
-
-void http_demo(NetworkInterface *net)
+void messageArrived(MQTT::MessageData& md)
 {
-    TCPSocket socket;
-
-    printf("Sending HTTP request to api.ubirch.com...\r\n");
-
-    // Open a socket on the network interface, and create a TCP connection to www.arm.com
-    socket.open(net);
-
-    socket.connect("api.ubirch.com", 80);
-
-    // Send a simple http request
-    char sbuffer[] = "GET / HTTP/1.1\r\n\r\n";
-
-    int scount = socket.send(sbuffer, sizeof sbuffer);
-    printf("sent %d [%.*s]\r\n", scount, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
-
-    // Recieve a simple http response and print out the response line
-    char rbuffer[64];
-    int rcount = socket.recv(rbuffer, sizeof rbuffer);
-    printf("recv %d [%.*s]\r\n", rcount, strstr(rbuffer, "\r\n")-rbuffer, rbuffer);
-
-    // Close the socket to return its memory and bring down the network interface
-    socket.close();
+    MQTT::Message &message = md.message;
+    logMessage("Message arrived: qos %d, retained %d, dup %d, packetid %d\r\n", message.qos, message.retained, message.dup, message.id);
+    logMessage("Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+    ++arrivedcount;
 }
 
-void modem_thread(void const *args) {
 
-    printf("M66 example\r\n\r\n");
+int main(int argc, char* argv[])
+{
+    float version = 0.6;
+    char* topic = "mbed-sample";
 
-    printf("\r\nConnecting...\r\n");
-    int ret = modem.connect("eseye.com", "ubirch", "internet");
-    if (ret != 0) {
-        printf("\r\nConnection error\r\n");
-    }
-    else {
-        printf("Success\r\n\r\n");
-        printf("IP: %s\r\n", modem.get_ip_address());
+    logMessage("HelloMQTT: version is %.2f\r\n", version);
 
-        http_demo(&modem);
+    M66Interface network(GSM_UART_TX, GSM_UART_RX, GSM_PWRKEY, GSM_POWER, true);
 
-        modem.disconnect();
+    network.connect("eseye.com", "ubirch", "internet");
 
-        printf("\r\nDone\r\n");
-    }
-}
+//    if (!network) {
+//        return -1;
+//    }
 
-osThreadDef(led_thread,   osPriorityNormal, DEFAULT_STACK_SIZE);
-//osThreadDef(bme_thread,   osPriorityNormal, DEFAULT_STACK_SIZE);
-//osThreadDef(modem_thread, osPriorityNormal, DEFAULT_STACK_SIZE);
+    MQTTNetwork mqttNetwork(&network);
 
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
-int main() {
+    MQTT::Client<MQTTNetwork, Countdown> client = MQTT::Client<MQTTNetwork, Countdown>(mqttNetwork);
 
-    printf("THREADS!\r\n");
+    const char* hostname = "iot.eclipse.org";
+    int port = 1883;
+    logMessage("Connecting to %s:%d\r\n", hostname, port);
+    int rc = mqttNetwork.connect(hostname, port);
+    if (rc != 0)
+    logMessage("rc from TCP connect is %d\r\n", rc);
 
-    osThreadCreate(osThread(led_thread), NULL);
-//    osThreadCreate(osThread(bme_thread), NULL);
-//    osThreadCreate(osThread(modem_thread), NULL);
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = "mbed-icraggs";
+    data.username.cstring = "username";
+    data.password.cstring = "password";
+    if ((rc = client.connect(data)) != 0)
+    logMessage("rc from MQTT connect is %d\r\n", rc);
 
-    printf("M66 example\r\n\r\n");
+    if ((rc = client.subscribe(topic, MQTT::QOS2, messageArrived)) != 0)
+    logMessage("rc from MQTT subscribe is %d\r\n", rc);
 
-    printf("\r\nConnecting...\r\n");
-    int ret = modem.connect("eseye.com", "ubirch", "internet");
-    if (ret != 0) {
-        printf("\r\nConnection error\r\n");
-    }
-    else {
-        printf("Success\r\n\r\n");
-        printf("IP: %s\r\n", modem.get_ip_address());
+    MQTT::Message message;
 
-        http_demo(&modem);
+    // QoS 0
+    char buf[100];
+    sprintf(buf, "Hello World!  QoS 0 message from app version %f\r\n", version);
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
+    rc = client.publish(topic, message);
+    while (arrivedcount < 1)
+        client.yield(100);
 
-        modem.disconnect();
+    // QoS 1
+    sprintf(buf, "Hello World!  QoS 1 message from app version %f\r\n", version);
+    message.qos = MQTT::QOS1;
+    message.payloadlen = strlen(buf)+1;
+    rc = client.publish(topic, message);
+    while (arrivedcount < 2)
+        client.yield(100);
 
-        printf("\r\nDone\r\n");
-    }
+    // QoS 2
+    sprintf(buf, "Hello World!  QoS 2 message from app version %f\r\n", version);
+    message.qos = MQTT::QOS2;
+    message.payloadlen = strlen(buf)+1;
+    rc = client.publish(topic, message);
+    while (arrivedcount < 3)
+        client.yield(100);
 
-    while (true) {
-      led1 = !led1;
-      Thread::wait(200);
-    }
+    if ((rc = client.unsubscribe(topic)) != 0)
+    logMessage("rc from unsubscribe was %d\r\n", rc);
+
+    if ((rc = client.disconnect()) != 0)
+    logMessage("rc from disconnect was %d\r\n", rc);
+
+    mqttNetwork.disconnect();
+
+    logMessage("Version %.2f: finish %d msgs\r\n", version, arrivedcount);
+
+    return 0;
 }
